6185c0f3f54597a6ade4d5123c7c8a6cd1b4ae1e
zfkun <zfkun@msn.com>
2013-10-13 00:53:15 +0800

    init
    init

---
+++ src/cookie.js
@@ -0,0 +1,115 @@
+/**
+ * Saber
+ * 
+ * @file Cookie
+ * @author zfkun(zfkun@msn.com)
+ */
+
+define(function () {
+
+    var exports = {};
+
+    /**
+     * 设置Cookie的值
+     * 
+     * @public
+     * @param {string} name Cookie名
+     * @param {string} value Cookie值
+     * @param {Object=} options Cookie选项
+     */
+    exports.set = function( name, value, options ) {
+        options = options || {};
+
+        // expires, 单位毫秒
+        var date = options.expires;
+        if ( 'number' === typeof date ) {
+            date = new Date();
+            date.setTime( date.getTime() + options.expires );
+        }
+
+        document.cookie =
+            name + '=' + ( options.raw ? value : encodeURIComponent( value ) )
+            + ( date instanceof Date ? '; expires=' + date.toUTCString() : '' )
+            + ( options.domain ? '; domain=' + options.domain : '' )
+            + ( options.path ? '; path=' + options.path : '' )
+            + ( options.secure ? '; secure' : '' );
+    };
+
+    /**
+     * 获取Cookie的值
+     * 
+     * @public 
+     * @param {string} name Cookie名
+     * @param {Object=} options Cookie选项
+     * @return {?string} 获取的Cookie值，获取不到时返回null
+     */
+    exports.get = function( name, options ) {
+        options = options || {};
+        return parseCookie( isValidName( name ) ? name : '', !options.raw );
+    };
+
+    /**
+     * 删除Cookie
+     * 
+     * @param {string} name 需要删除Cookie的键名
+     * @param {Object=} options 需要删除Cookie的配置
+     */
+    exports.remove = function( name, options ) {
+        options = options || {};
+        options.expires = new Date(0);
+        this.set( name, '', options );
+    };
+
+
+    /**
+     * 空字符串检查
+     * 
+     * @inner
+     * @param {?string} str 目标字符串
+     * @return {boolean}
+     */
+    function isNotEmptyString( str ) {
+        return typeof str === 'string' && str !== '';
+    }
+
+    /**
+     * 验证字符串是否合法的cookie键名
+     * 
+     * @param {string} name 待验证的键名字符串
+     * @return {boolean}
+     */
+    function isValidName( name ) {
+        return isNotEmptyString( name );
+    }
+
+
+    /**
+     * 解析Cookie
+     * 
+     * @param {string} name Cookie名
+     * @param {boolean} needDecod 是否自动解码
+     * @return {?string} 获取的Cookie值，获取不到时返回null
+     */
+    function parseCookie( name, needDecod ) {
+        if ( isNotEmptyString( name ) ) {
+            var matchs = String( document.cookie ).match(
+                new RegExp( '(?:^| )' + name + '(?:(?:=([^;]*))|;|$)' )
+            );
+
+            if ( matchs ) {
+                if ( (matchs = matchs[1]) ) {
+                    return needDecod ? decodeURIComponent( matchs ) : matchs;
+                }
+
+                return '';
+            }
+
+            return null;
+        }
+
+        return null;
+    }
+
+    return exports;
+
+});
---
+++ test/cookie.test.js
@@ -0,0 +1,141 @@
+define(function() {
+
+    Cookie = require( 'saber-cookie' );
+
+    describe( 'Cookie', function() {
+
+        var rawText = 'Hi，百度！ !@#%$(~...';
+
+        describe( 'get', function() {
+
+            document.cookie = '__saber_g1=1';
+            document.cookie = '__saber_g2';
+            document.cookie = '__saber_g3=';
+            document.cookie = '__saber_g4[abc]=x';
+            document.cookie = '__saber_g5=' + encodeURIComponent( rawText );
+
+            it( 'should return string value for the given name.', function() {
+
+                expect( Cookie.get( '__saber_g1' ) ).toEqual( '1' );
+                expect( Cookie.get( '__saber_g2' ) ).toEqual( '' );
+                expect( Cookie.get( '__saber_g3' ) ).toEqual( '' );
+                expect( Cookie.get( '__saber_g5' ) ).toEqual( rawText );
+
+            });
+
+            it( 'should return null for like-array name.', function() {
+
+                expect( Cookie.get( '__saber_g4' ) ).toEqual( null );
+
+            });
+
+
+            it( 'should return null for non-existing name.', function() {
+
+                expect( Cookie.get( '__saber_g4' ) ).toEqual( null );
+                expect( Cookie.get( '__saber_gx' ) ).toEqual( null );
+
+            });
+
+            it(
+                'should return raw value for the given name'
+                + ' when `options.raw = true`.',
+                function() {
+
+                    expect(
+                        Cookie.get( '__saber_g5', { raw: true } )
+                    ).toEqual( encodeURIComponent( rawText ) );
+
+                }
+            );
+
+        });
+
+
+        describe( 'set', function() {
+
+            var rawText = 'Hi，百度一下，你就知道！';
+
+            it( 'should set a cookie with given name and value.', function() {
+
+                Cookie.set( '__saber_s1', 'a' );
+                expect( Cookie.get( '__saber_s1' ) ).toEqual( 'a' );
+                expect( Cookie.get( '__saber_s1', { raw: true } ) ).toEqual(
+                    encodeURIComponent( 'a' )
+                );
+
+                Cookie.set( '__saber_s2', 'b', { expires: -1 } );
+                expect( Cookie.get( '__saber_s2' ) ).toEqual( null );
+                expect( Cookie.get( '__saber_s2', { raw: true } ) ).toEqual(
+                    null
+                );
+
+                Cookie.set( '__saber_s3', 'c', {
+                    expires: new Date( 2020, 1, 1 ),
+                    path: '/'
+                });
+                expect( Cookie.get( '__saber_s3' ) ).toEqual( 'c' );
+                expect( Cookie.get( '__saber_s3', { raw: true } ) ).toEqual(
+                    encodeURIComponent( 'c' )
+                );
+
+                Cookie.remove( '__saber_s4' );
+                Cookie.set( '__saber_s4', 'd', {
+                    domain: document.domain,
+                    path: '/',
+                    secure: true
+                });
+                expect( Cookie.get( '__saber_s4' ) ).toEqual( null );
+                expect( Cookie.get( '__saber_s4', { raw: true } ) ).toEqual(
+                    null
+                );
+
+                Cookie.set( '__saber_s5', rawText );
+                expect( Cookie.get( '__saber_s5' ) ).toEqual( rawText );
+                expect( Cookie.get( '__saber_s5', { raw: true } ) ).toEqual(
+                    encodeURIComponent( rawText )
+                );
+
+            });
+
+            it(
+                'should set a cookie with given name and value'
+                + ' when `options.raw = true`.',
+                function() {
+
+                    Cookie.set( '__saber_s6', rawText, { raw: true } );
+                    expect( Cookie.get( '__saber_s6' ) ).toEqual(
+                        rawText
+                    );
+                    expect( Cookie.get( '__saber_s6', { raw: true } ) ).toEqual(
+                        rawText
+                    );
+
+                }
+            );
+
+        });
+
+
+        describe( 'remove', function() {
+
+            it( 'should remove a cookie with given name.', function() {
+
+                Cookie.set( '__saber_r1', 'x' );
+                Cookie.remove( '__saber_r1' );
+                expect( Cookie.get( '__saber_r1' ) ).toEqual( null );
+
+                Cookie.set( '__saber_r2', 'y', {
+                    expires: new Date( 2020, 1, 1 ),
+                    path: '/'
+                });
+                Cookie.remove( '__saber_r2', { path: '/' } );
+                expect( Cookie.get( '__saber_r2' ) ).toEqual( null );
+
+            });
+
+        });
+
+    });
+
+});
